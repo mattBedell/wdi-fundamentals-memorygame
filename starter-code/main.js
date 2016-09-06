d3070c37ef697e6f5fd08d1deb21bfb02b4ecbf6
Matt Bedell <matt@bedell.co>
2016-09-06 17:12:51 -0400

    added createBoard function to .js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,19 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+var creatBoard = function(cardNum){
+	for(var i = 0; i < cardNum; i++){
+		var cardPop = document.createElement('div');
+		cardPop.className = 'card';
+		document.getElementsByClassName('board')[0].appendChild(cardPop);
+	}
+}
+creatBoard(4);
+
+
+/*var cardPop = document.createElement('div');
+cardPop.className = 'card';
+document.getElementsByClassName('board')[0].appendChild(cardPop);
+*/
