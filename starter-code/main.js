349b8a9c6ae8659bd44a78a4e114f4aae5bb2f2c
Matt Bedell <Matt@MBMB.local>
2016-09-11 20:27:55 -0400

    Match function, game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,7 @@
 var cardVal = ['queen', 'queen', 'king', 'king'];
+var cardClick = [];
 var cardsInPlay = [];
+
 
 
 var createBoard = function(){
@@ -9,24 +11,44 @@ var createBoard = function(){
 		document.getElementsByClassName('board')[0].appendChild(cardPop);
 
 		cardPop.setAttribute("data-card", cardVal[i]);
-		cardPop.addEventListener('click', isTwoCards);
+		cardPop.setAttribute("card-sel", "cardNum" + i);
+
+		cardPop.addEventListener('click', cardControl);
+		cardPop.addEventListener('click', isMatch);
+
 	}
 }
 
 
 var isMatch = function(){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		return true;
-	}else{
-		return false;
+	if(cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			cardClick[0].style.opacity = "0.5";
+			cardClick[1].style.opacity = "0.5";
+			cardsInPlay = [];
+			cardClick = [];
+			return true;
+		}else{
+			//cardClick[0].style.backgroundImage = '';
+			//cardClick[1].style.backgroundImage = '';
+			cardsInPlay = [];
+			cardClick = [];
+			return false;
+		}
 	}
 }
 
-var isTwoCards = function(){
+var cardControl = function(){
 	cardsInPlay.push(this.getAttribute('data-card'));
-	if(cardsInPlay.length === 2){
-		isMatch();//WHAT???????
-		cardsInPlay = [];
+	cardClick.push(this);
+
+	switch(this.getAttribute('data-card')){
+		case 'queen':
+			this.style.backgroundImage = "url('assets/queenImage.png')";
+			break;
+		case 'king':
+			this.style.backgroundImage = "url('assets/kingImage.png')";
+			break;
 	}
 }
 
